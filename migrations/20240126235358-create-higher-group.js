a8a0b6b5270a703a2cb455560955a2350db795aa
james yates <yates_7@hotmail.com>
2024-01-27 03:23:04 +0000

    fix table names to allow migrations to marry up

--- migrations/20240126235358-create-higher-group.js
+++ migrations/20240126235358-create-higher-group.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize, DataTypes) {
-    await queryInterface.createTable('HigherGroups', {
+    await queryInterface.createTable('HigherGroup', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -24,6 +24,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('HigherGroups');
+    await queryInterface.dropTable('HigherGroup');
   }
 };
--- models/highergroup.js
+++ models/highergroup.js
@@ -28,6 +28,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }, {
     sequelize,
+    freezeTableName: true,
     tableName: 'HigherGroup',
     modelName: 'HigherGroup',
   }
